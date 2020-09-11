4faf14236cfd800c8141916ed1d3e4499452cdfa
Javiuty <javiiescuadra@gmail.com>
2020-09-11 19:11:13 +0200

    first commit

---
+++ functions/movies.js
@@ -0,0 +1,34 @@
+const { URL } = require('url');
+const fetch = require('node-fetch');
+const movies = require('../data/movies.json');
+
+exports.handler = async () => {
+    const api = new URL('https://www.omdbapi.com/');
+
+    // add secret api key to the query string
+    api.searchParams.set('apikey', process.env.OMDB_API_KEY)
+
+    const promises = movies.map((movie) => {
+        // use the movie's IMDB id to look up details
+        api.searchParams.set('i', movie.id);
+
+        return fetch(api)
+            .then((response) => response.json())
+            .then((data) => {
+                const scores = data.Ratings;
+
+                return {
+                    ...movie,
+                    scores,
+                };
+
+            });
+    });
+
+    const moviesWithRatings = await Promise.all(promises)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(moviesWithRatings)
+    }
+}
